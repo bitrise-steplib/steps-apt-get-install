91d73e68dff4a6b89f1ee7530b882e326d95fcd4
Richard Bogdan <richard.bogdan@bitrise.io>
2019-10-30 17:05:10 +0100

    TOOL-1217 Review fix.

--- main.go
+++ main.go
@@ -1,29 +1,26 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"strings"
 
 	"github.com/bitrise-io/go-steputils/stepconf"
 	"github.com/bitrise-io/go-utils/command"
-	"github.com/bitrise-io/go-utils/fileutil"
 	"github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-tools/go-steputils/cache"
 	"github.com/kballard/go-shellquote"
 )
 
 const (
-	cacheInputNone = "none"
-	cacheInputAll  = "all"
+	cacheInputAll = "all"
 )
 
-// ConfigsModel ...
-type ConfigsModel struct {
+// configsModel ...
+type configsModel struct {
 	Packages   string `env:"packages,required"`
 	Options    string `env:"options"`
-	Upgrade    string `env:"upgrade"`
+	Upgrade    string `env:"upgrade,opt[yes,no]"`
 	CacheLevel string `env:"cache_level,opt[all,none]"`
 }
 
@@ -32,37 +29,15 @@ func fail(format string, v ...interface{}) {
 	os.Exit(1)
 }
 
-func (configs ConfigsModel) print() {
-	log.Infof("Configs:")
-	log.Printf("- Packages: %s", configs.Packages)
-	log.Printf("- Options: %s", configs.Options)
-	log.Printf("- Upgrade: %s", configs.Upgrade)
-	log.Printf("- Cache Level: %s", configs.CacheLevel)
-}
-
-func (configs ConfigsModel) validate() error {
-	if configs.Packages == "" {
-		return errors.New("no Packages parameter specified")
-	}
-	if configs.Upgrade != "" && configs.Upgrade != "yes" && configs.Upgrade != "no" {
-		return fmt.Errorf("invalid 'Upgrade' specified (%s), valid options: [yes no]", configs.Upgrade)
-	}
-	return nil
-}
-
 func main() {
-	var configs ConfigsModel
+	var configs configsModel
 	if err := stepconf.Parse(&configs); err != nil {
 		fail("Issue with input: %s", err)
 	}
 
 	fmt.Println()
-	configs.print()
+	stepconf.Print(configs)
 	fmt.Println()
-
-	if err := configs.validate(); err != nil {
-		fail("Issue with input: %s", err)
-	}
 
 	if err := applyCacheConfig(configs.CacheLevel); err != nil {
 		fail("Could not apply caching: %s", err)
@@ -97,25 +72,8 @@ func main() {
 }
 
 func applyCacheConfig(cacheConfig string) error {
-	switch cacheConfig {
-	case cacheInputNone:
-		return applyNoCache()
-	case cacheInputAll:
+	if cacheConfig == cacheInputAll {
 		return applyAllCache()
-	default:
-		return fmt.Errorf("invalid cache level, no such configuration: %s", cacheConfig)
-	}
-}
-
-func applyNoCache() error {
-	if err := disablePackageCache(); err != nil {
-		return fmt.Errorf("could not disable package cache: %s", err)
-	}
-	if err := removeDownloadedPackages(); err != nil {
-		return fmt.Errorf("could not remove downloaded package cache: %s", err)
-	}
-	if err := removeLookupFiles(); err != nil {
-		return fmt.Errorf("could not remove lookup files: %s", err)
 	}
 	return nil
 }
@@ -130,20 +88,6 @@ func applyAllCache() error {
 		return fmt.Errorf("could not add packages to cache: %s", err)
 	}
 	return nil
-}
-
-func disablePackageCache() error {
-	cacheFilePth := "/etc/apt/apt.conf.d/00_disable-cache"
-	cacheFileCnt := `Dir::Cache::pkgcache "";` + "\n" + `Dir::Cache::srcpkgcache "";` + "\n" + `Dir::Cache "";` + "\n" + `Dir::Cache::archives "";`
-	return fileutil.AppendStringToFile(cacheFilePth, cacheFileCnt)
-}
-
-func removeDownloadedPackages() error {
-	return command.RemoveAll("/var/cache/apt/archives/")
-}
-
-func removeLookupFiles() error {
-	return command.RemoveAll("/var/cache/apt/pkgcache.bin", "/var/cache/apt/srcpkgcache.bin")
 }
 
 func removeDockerCleanFile() error {
