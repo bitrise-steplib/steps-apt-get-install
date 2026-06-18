ca961c551547757e569e46f9aaa4932c13638672
Olivér Falvai <ofalvai@gmail.com>
2026-06-18 12:24:48 +0200

    *

--- main.go
+++ main.go
@@ -47,9 +47,9 @@ func main() {
 
 	var cmdArgs []string
 	if configs.Upgrade == "yes" {
-		cmdArgs = append(cmdArgs, "upgrade", "-y")
+		cmdArgs = append(cmdArgs, "apt-get", "upgrade", "-y")
 	} else {
-		cmdArgs = append(cmdArgs, "install", "-y")
+		cmdArgs = append(cmdArgs, "apt-get", "install", "-y")
 	}
 	if configs.Options != "" {
 		args, err := shellquote.Split(configs.Options)
@@ -63,7 +63,7 @@ func main() {
 
 	fmt.Println()
 	log.Infof("$ apt-get %s", command.PrintableCommandArgs(false, cmdArgs))
-	if err := command.RunCommand("sudo", "apt-get", cmdArgs...); err != nil {
+	if err := command.RunCommand("sudo", cmdArgs...); err != nil {
 		fail("Can't install packages:  %s", err)
 	}
 }
